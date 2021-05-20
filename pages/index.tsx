881d92f44b9c8a4c318acc164bbfd40774dd9da7
Boonyarith Piriyothinkul <boonyarith.pir@gmail.com>
2021-05-21 02:47:10 +0700

    first version

---
+++ pages/index.tsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+type Vaccine = { label: string; photoUrl: string };
+
+const VACCINES: Vaccine[] = [
+  {
+    label: "Astrazeneca",
+    photoUrl: "/az.png",
+  },
+  {
+    label: "Sinovac",
+    photoUrl: "/sinovac.png",
+  },
+];
+
+function random() {
+  return Math.floor(Math.random() * 100);
+}
+
+function getRandomVac() {
+  return random() <= 20 ? VACCINES[0] : VACCINES[1];
+}
+
+function rememberEngage() {
+  localStorage.setItem("visit_at", Date.now().toString());
+  let countStr = localStorage.getItem("count") || "0";
+  let count = parseInt(countStr);
+  count += 1;
+  localStorage.setItem("count", count.toString());
+}
+
+function shouldWaitLonger() {
+  let visitAt = localStorage.getItem("visit_at");
+  let countStr = localStorage.getItem("count") || "0";
+  let count = parseInt(countStr);
+
+  if (visitAt === null || count % 3 != 0) return false;
+
+  let difference = Date.now() - Number.parseInt(visitAt);
+  let visitAtTimestamp = new Date(difference);
+  let minutes = visitAtTimestamp.getMinutes();
+  console.log(visitAt, minutes);
+  return minutes < 10 ? true : false;
+}
+
+export default function Home() {
+  const [state, setState] = useState("WELCOME");
+  const [vac, setVac] = useState<Vaccine | null>(null);
+
+  useEffect(() => {
+    let randomVac = getRandomVac();
+    setVac(randomVac);
+  }, []);
+
+  const handleClick = () => {
+    if (shouldWaitLonger()) {
+      setState("WAIT");
+      return;
+    }
+
+    setState("RESULT");
+    rememberEngage();
+  };
+
+  return (
+    <div className={styles.container}>
+      {state === "WAIT" && (
+        <div className={styles.content}>
+          <h1>
+            ขออภัยด้วย ตอนนี้ยาหมดแล้ว พอดีรัฐบาลนำเข้ามาไม่ทัน
+            อีกสักพักกลับมาใหม่นะ <br /> 😥😥😥
+          </h1>
+        </div>
+      )}
+
+      {state === "WELCOME" && (
+        <div className={styles.content}>
+          <h1>เปิดกาชา! ฉีดวัคซีน covid-19 ครั้งนี้ คุณจะได้ฉีดอะไร!?</h1>
+          <button onClick={handleClick}>ฉีดเลย!</button>
+        </div>
+      )}
+
+      {state === "RESULT" && vac != null && (
+        <div className={styles.content}>
+          <h1>ยินดีด้วย! คุณได้ฉีด {vac.label} 🥳 </h1>
+          <div>
+            <Image
+              src={vac.photoUrl}
+              alt="covid-19 vaccine logo"
+              width="256"
+              height="256"
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
